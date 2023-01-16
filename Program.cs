a037373ae19810c9c2d54acb8fdbb1d4a861e329
valerya <reykon-kun@yandex.ru>
2023-01-16 13:15:15 +0200

    раскомитила

--- Program.cs
+++ Program.cs
@@ -3,21 +3,21 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// Console.WriteLine("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int[] array = new int[5];
-// Random rand = new Random();
-// int result = 0;
-// for (int i = 0; i < array.Length; i++)
-// {
-//     array[i] = new Random().Next(-567, 567);
-//     Console.Write($"{array[i]} ");
-// if (array[i] > 0)
-// {
-//     result = result + 1;
-// }
-// }
-// Console.WriteLine($"Кол-во элементов > 0: {result}");
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[5];
+Random rand = new Random();
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-567, 567);
+    Console.Write($"{array[i]} ");
+if (array[i] > 0)
+{
+    result = result + 1;
+}
+}
+Console.WriteLine($"Кол-во элементов > 0: {result}");
 
 
 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
